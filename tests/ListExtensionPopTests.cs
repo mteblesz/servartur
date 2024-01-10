9b4aadd762a2a59a4c1047ec452d3bac236e52c1
teblesz <mteblesz@gmail.com>
2024-01-10 18:42:03 +0100

    Add List.Pop tests

--- tests/ListExtensionPopTests.cs
+++ tests/ListExtensionPopTests.cs
@@ -1,11 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using servartur.Algorithms;
 
 namespace servartur.Tests;
-internal class ListExtensionPopTests
+public class ListExtensionPopTests
 {
-    //TODO
+    [Fact]
+    public void Pop_RemovesFirstElementFromList_ReturnsRemovedElement()
+    {
+        // Arrange
+        int elementToPop = 0;
+        List<int> backOfList = [1, 2, 3];
+        List<int> list = [elementToPop, ..backOfList];
+
+        // Act
+        var result = list.Pop();
+
+        // Assert
+        result.Should().Be(elementToPop);
+        list.Should().BeEquivalentTo(backOfList);
+    }
+
+    [Fact]
+    public void Pop_OnEmptyList_ThrowsException()
+    {
+        // Arrange
+        var emptyList = new List<int>();
+
+        // Act and Assert
+        Action action = () => emptyList.Pop();
+        Assert.Throws<InvalidOperationException>(action);
+    }
 }
