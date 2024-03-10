318af8f7f5abba0413be73cc94617d97e3028e7a
mteblesz <mteblesz@gmail.com>
2024-03-11 00:11:35 +0100

    Separate matchup rtu

---
+++ src/RealTimeUpdates/GameHubMatchupExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using servartur.Models;
+
+namespace servartur.RealTimeUpdates;
+
+using GameHubContext = IHubContext<GameHub, IGameHubClient>;
+
+// https://stackoverflow.com/a/74414966/23287406
+public static class GameHubMatchupExtensions
+{
+    public static async Task RefreshPlayers(this GameHubContext context, int roomId, List<PlayerInfoDto> updatedPlayers)
+    {
+        var groupName = roomId.ToString();
+        await context.Clients.Group(groupName).ReceivePlayerList(updatedPlayers);
+    }
+    public static async Task SendRemovalInfo(this GameHubContext context, int roomId, int playerId)
+    {
+        var groupName = roomId.ToString();
+        await context.Clients.Group(groupName).ReceiveRemoval(playerId.ToString());
+    }
+    public static async Task SendStartGame(this GameHubContext context, int roomId)
+    {
+        var groupName = roomId.ToString();
+        await context.Clients.Group(groupName).ReceiveStartGame();
+    }
+}
