348a44385fedf35de3b911c6ea051a83ebb3f761
mteblesz <mteblesz@gmail.com>
2024-03-11 03:14:42 +0100

    Change mapping acording to new dtos

--- src/Mappings/MappingProfile.cs
+++ src/Mappings/MappingProfile.cs
@@ -15,20 +15,28 @@ public class MappingProfile : Profile
         CreateMap<StartGameDto, GameStartHelper.RoleInfo>();
 
         CreateMap<Player, PlayerInfoDto>();
-        CreateMap<Player, PlayerRoleInfoDto>(); 
+        CreateMap<Player, PlayerRoleInfoDto>();
+
 
         CreateMap<SquadVote, SquadVoteInfoDto>()
-            .ForMember(dest => dest.VoterNick, opt => opt.MapFrom(src => src.Voter.Nick))
-            .ForMember(dest => dest.Value, opt => opt.MapFrom(src => src.Value));
+            .ForMember(dest => dest.VoterNick, opt => opt.MapFrom(
+                src => src.Voter.Nick))
+            .ForMember(dest => dest.Value, opt => opt.MapFrom(
+                src => src.Value));
 
         CreateMap<Squad, SquadInfoDto>()
             .ForMember(dest => dest.Members, opt => opt.MapFrom(
-                src =>  src.Memberships.Select(m => m.Player)
-                ))
+                src => src.Memberships.Select(m => m.Player) ?? Enumerable.Empty<Player>()));
+
+        CreateMap<Squad, QuestInfoShortDto>();
+
+        CreateMap<Squad, QuestInfoDto>()
+            .ForMember(dest => dest.Members, opt => opt.MapFrom(
+                src => src.Memberships.Select(m => m.Player) ?? Enumerable.Empty<Player>()))
+            .ForMember(dest => dest.SquadVoteInfo, opt => opt.MapFrom(
+                src => src.SquadVotes))
             .ForMember(dest => dest.QuestVoteSuccessCount, opt => opt.MapFrom(
-                src => src.QuestVotes.Count(qv => qv.Value)
-                ));
-    
+                src => src.QuestVotes.Count(qv => qv.Value)));
 
         CreateMap<CastVoteDto, SquadVote>();
         CreateMap<CastVoteDto, QuestVote>();
